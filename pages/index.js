8cdca10395e44976283427bb70ce071dfb68ac95
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-08-04 21:43:55 +0500

    fix: сократить 4 функции для popup до одной

--- pages/index.js
+++ pages/index.js
@@ -8,33 +8,25 @@ let formElement = document.querySelector('.popup__edit-form');
 let profileInputName = document.querySelector('.popup__edit-item_type_username');
 let profileInputDescr = document.querySelector('.popup__edit-item_type_description');
 
-let popupOpen = function () {
-  popup.classList.add('popup_opened');
+let popupToggle = function () {
+  if (popup.classList.contains('popup_opened')) {
+    popup.classList.remove('popup_opened');
+  } else {
+    profileInputName.value = profileName.textContent;
+    profileInputDescr.value = profileDescr.textContent;
+    popup.classList.add('popup_opened');
+  }
 }
 
-let popupClose = function () {
-  popup.classList.remove('popup_opened');
-}
-
-function getProfile() {
-  profileInputName.value = profileName.textContent;
-  profileInputDescr.value = profileDescr.textContent;
-}
-
-function saveProfile (event) {
+let saveProfile = function (event) {
   event.preventDefault();
   profileName.textContent = profileInputName.value;
   profileDescr.textContent = profileInputDescr.value;
-  popupClose();
+  popupToggle();
 }
 
-function editProfile() {
-  getProfile();
-  popupOpen();
-}
-
-popupOpenButton.addEventListener('click', editProfile);
-popupCloseButton.addEventListener('click', popupClose);
+popupOpenButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
 formElement.addEventListener('submit', saveProfile);
-popupOverlay.addEventListener('click', popupClose);
+popupOverlay.addEventListener('click', popupToggle);
 
