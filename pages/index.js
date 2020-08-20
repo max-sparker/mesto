754a00ad4a294fb5e17fe87749c61be3d7298c83
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-08-20 10:51:46 +0500

    fix: разбиение функции addCardToContainer на однозначные

--- pages/index.js
+++ pages/index.js
@@ -36,6 +36,9 @@ const formProfileElement = popupProfile.querySelector('.popup__form');
 const profileInputName = formProfileElement.querySelector('.popup__input_type_username');
 const profileInputDescr = formProfileElement.querySelector('.popup__input_type_description');
 
+// Контейнер для карточек
+const placeContainer = document.querySelector('.places');
+
 const popupProfileToggle = () => {
   if (popupProfile.classList.contains('popup_opened')) {
     popupProfile.classList.remove('popup_opened');
@@ -58,30 +61,59 @@ popupProfileCloseButton.addEventListener('click', popupProfileToggle);
 formProfileElement.addEventListener('submit', saveProfile);
 popupProfileOverlay.addEventListener('click', popupProfileToggle);
 
-// Добавление карточки
-const placeContainer = document.querySelector('.places');
-const addCardToContainer = card => {
-  const cardElement = document.querySelector('#cardTemplate').content.cloneNode(true);
-  const cardImage = cardElement.querySelector('.card__image');
-  cardImage.src = card.link;
-  cardImage.alt = `Фото ${card.name}`;
-  cardElement.querySelector('.card__name').textContent = card.name;
-  cardElement.querySelector('.card__remove-btn').addEventListener('click', (evt) => {
-    const card = evt.target.closest('.card');
-    card.remove();
-  })
-  cardElement.querySelector('.card__like-btn').addEventListener('click', (evt) => {
-    evt.target.classList.toggle('card__like-btn_liked');
-  })
-  cardElement.querySelector('.card__image').addEventListener('click', () => {
-    popupImageEl.src = card.link;
-    popupImageEl.alt = `Фото ${card.name}`;
-    popupImageDesc.textContent = card.name;
-    popupImageToggle();
-  })
-  placeContainer.prepend(cardElement);
+// рендер карточки
+const renderCard = card => {
+  placeContainer.prepend(card);
 }
-initialCards.forEach(addCardToContainer);
+
+// постановка лайка
+const handleLikeIcon = (evt) => {
+  evt.target.classList.toggle('card__like-btn_liked');
+}
+
+// удаление карточки
+const handleDeleteCard = (evt) => {
+  const card = evt.target.closest('.card');
+  card.remove();
+}
+
+// демонстрация изображения
+const handlePreviewPicture = (evt) => {
+  popupImageEl.src = evt.target.src;
+  popupImageEl.alt = `Фото ${evt.target.alt}`;
+  popupImageDesc.textContent = evt.target.alt;
+  popupImageToggle();
+}
+
+const getCardElement = (name, link) => {
+  const cardElement = document.querySelector('#cardTemplate').content.cloneNode(true);
+
+  const cardImage = cardElement.querySelector('.card__image');
+  cardImage.src = link;
+  cardImage.alt = `Фото ${name}`;
+
+  cardImage.addEventListener('click', handlePreviewPicture);
+
+  const cardName = cardElement.querySelector('.card__name');
+  cardName.textContent = name;
+
+  const deleteButton = cardElement.querySelector('.card__remove-btn');
+  deleteButton.addEventListener('click', handleDeleteCard);
+
+  const likeButton = cardElement.querySelector('.card__like-btn');
+  likeButton.addEventListener('click', handleLikeIcon);
+
+  return cardElement;
+}
+
+const fillInitialCards = () => {
+  initialCards.forEach((card) => {
+    let cardElement = getCardElement (card.name, card.link);
+    renderCard(cardElement);
+  })
+}
+
+fillInitialCards();
 
 const popupCard = document.querySelector('.popup-card');
 const popupCardOpenButton = document.querySelector('.profile__add-btn');
@@ -96,12 +128,16 @@ const popupCardToggle = () => {
 popupCardOpenButton.addEventListener('click', popupCardToggle);
 popupCardCloseButton.addEventListener('click', popupCardToggle);
 popupCardOverlay.addEventListener('click', popupCardToggle);
-formCardElement.addEventListener('submit', evt => {
-  evt.preventDefault();
+
+const createCard = () => {
   const cardTitle = formCardElement.querySelector('.popup__input_type_placename').value;
   const cardLink = formCardElement.querySelector('.popup__input_type_placelink').value;
-  const card = {'name':cardTitle, 'link':cardLink};
-  addCardToContainer(card);
+  renderCard(getCardElement(cardTitle, cardLink));
+}
+
+formCardElement.addEventListener('submit', evt => {
+  evt.preventDefault();
+  createCard();
   formCardElement.reset();
   popupCardToggle();
 })
