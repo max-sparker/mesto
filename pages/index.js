faee708a78109afde7d7f51fb8e6756263c2b3b6
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-09-01 01:01:05 +0500

    fix: измение класса выключенной кнопки popup__btn_disabled

--- pages/index.js
+++ pages/index.js
@@ -178,3 +178,12 @@ popupImageOverlay.addEventListener('click', () => {
   toggleModalWindow(popupImage);
   clearImageDescription();
 });
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__btn',
+  inactiveButtonClass: 'popup__btn_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
