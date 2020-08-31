5b06bd7cc3964f848ac91faa5c9bb61aa63f3a43
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-08-31 10:49:32 +0500

    feat: добавлена функция включения валидации для форм

---
+++ pages/validate.js
@@ -0,0 +1,8 @@
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+    });
+  });
+}
