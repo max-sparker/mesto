eec905e638064faa2150be4703407ededb214b6f
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-09-01 02:05:59 +0500

    feat: добавлена функция перебора полей

--- pages/index.js
+++ pages/index.js
@@ -179,11 +179,4 @@ popupImageOverlay.addEventListener('click', () => {
   clearImageDescription();
 });
 
-enableValidation({
-  formSelector: '.popup__form',
-  inputSelector: '.popup__input',
-  submitButtonSelector: '.popup__btn',
-  inactiveButtonClass: 'popup__btn_disabled',
-  inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
-});
+
--- pages/validate.js
+++ pages/validate.js
@@ -1,8 +1,29 @@
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.popup__form'));
+
+
+const setEventListeners = (formElement, {inputSelector, ...rest}) => {
+  const inputList = Array.from(formElement.querySelectorAll(inputSelector));
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+
+    });
+  });
+}
+
+const enableValidation = ({formSelector, ...rest}) => {
+  const formList = Array.from(document.querySelectorAll(formSelector));
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', function (evt) {
       evt.preventDefault();
     });
+    setEventListeners(formElement, rest);
   });
 }
+
+enableValidation({
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__btn',
+  inactiveButtonClass: 'popup__btn_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__error_visible'
+});
