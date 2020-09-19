c3e507ecd2b6118e3ae9365a7f70453c6aef4ac4
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-09-19 20:16:01 +0500

    feat: Пустой метод установки слушателей в классе Card

--- pages/Card.js
+++ pages/Card.js
@@ -7,12 +7,31 @@ export class Card {
 
   _getTemplate() {
     const cardElement = document.querySelector(this._cardSelector).content.cloneNode(true);
-
     return cardElement;
+  }
+
+
+  _setEventListeners() {
+    const cardImage = this._element.querySelector('.card__image');
+    const cardLikeBtn = this._element.querySelector('.card__like-btn');
+    const cardDeleteBtn = this._element.querySelector('.card__remove-btn');
+
+    cardImage.addEventListener('click', () => {
+
+    });
+
+    cardLikeBtn.addEventListener('click', () => {
+
+    });
+
+    cardDeleteBtn.addEventListener('click', () => {
+
+    });
   }
 
   createCard() {
     this._element = this._getTemplate();
+    this._setEventListeners();
 
     const cardImage = this._element.querySelector('.card__image');
     cardImage.src = this._link;
