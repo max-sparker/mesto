2d40b6b089b8055ba100b95513d9de16ab63fe8f
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-09-19 20:06:08 +0500

    feat: Функция создания карточки в классе Card

--- pages/Card.js
+++ pages/Card.js
@@ -1,18 +1,27 @@
-export default class Card {
+export class Card {
   constructor(name, link, cardSelector) {
     this._cardSelector = cardSelector;
-    this.name = name;
-    this.link = link;
+    this._name = name;
+    this._link = link;
   }
 
   _getTemplate() {
-    const cardElement = document
-      .querySelector(this._cardSelector)
-      .content
-      .querySelector('#cardTemplate')
-      .cloneNode(true);
+    const cardElement = document.querySelector(this._cardSelector).content.cloneNode(true);
 
     return cardElement;
+  }
+
+  createCard() {
+    this._element = this._getTemplate();
+
+    const cardImage = this._element.querySelector('.card__image');
+    cardImage.src = this._link;
+    cardImage.alt = `Фото ${this._name}`;
+
+    const cardName = this._element.querySelector('.card__name');
+    cardName.textContent = this._name;
+
+    return this._element;
   }
 
 }
