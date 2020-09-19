0c19c585b914cf57d152731729645f30cc2237b5
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-09-19 19:06:34 +0500

    feat: экспорт и импорт Card

--- pages/Card.js
+++ pages/Card.js
@@ -1,7 +1,18 @@
-class Card {
+export default class Card {
   constructor(name, link, cardSelector) {
     this._cardSelector = cardSelector;
     this.name = name;
     this.link = link;
   }
+
+  _getTemplate() {
+    const cardElement = document
+      .querySelector(this._cardSelector)
+      .content
+      .querySelector('#cardTemplate')
+      .cloneNode(true);
+
+    return cardElement;
+  }
+
 }
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Card from './Card';
+
 const initialCards = [
   {
     name: 'Архыз',
