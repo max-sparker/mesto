b22b9f45a60a58d3229c7842e898ee580dda0940
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-09-21 09:30:06 +0500

    feat: вывод первоначальных данных в отдельый файл

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,7 @@
 import Card from './Card.js';
 import FormValidator from './FormValidator.js';
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
+
+import {initialCards, formConfig} from './constants.js';
 
 // Профиль
 const popupProfile = document.querySelector('.popup-profile');
