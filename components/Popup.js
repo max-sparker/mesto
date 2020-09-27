82155d3401911da2ce9278a32f3ff089e391c95d
Maksim Isaev <maksim.a.isaev@gmail.com>
2020-09-27 11:29:51 +0500

    feat: создание и описание класса Popup

---
+++ components/Popup.js
@@ -0,0 +1,33 @@
+import {keyClose} from "../utils/constants.js";
+
+class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === keyClose) {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popup.querySelector('.popup__close-btn').addEventListener('click', () => {
+      this.close();
+    });
+    this._popup.querySelector('.popup__overlay').addEventListener('click', () => {
+      this.close();
+    })
+  }
+
+}
